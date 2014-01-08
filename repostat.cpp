712cccc293ef9e9d71edbd372be9f039636e3376
Drew Guarnera <dtg3@zips.uakron.edu>
2014-01-07 19:56:54 -0500

    Support Git Rev Walking

--- repostat.cpp
+++ repostat.cpp
@@ -1,13 +1,26 @@
 #include <boost/filesystem.hpp>
 #include <git2.h>
 
-// g++ repostat.cpp -lboost_filesystem -lboost_system -llibgit2 -o rs
+// g++ repostat.cpp -lboost_filesystem -lboost_system -lgit2 -o rs
 int main(int argc, char * argv[]) {
 	if (boost::filesystem::create_directory("repos"))
 		std::cerr << "Done!\n";
 
 	git_repository *repo = NULL;
 	const char *url = "https://github.com/octocat/Spoon-Knife";
-	const char *path = "repos";
+	const char *path = "repos/spoon";
 	int error = git_clone(&repo, url, path, NULL);
+	// Check for error
+
+	git_revwalk *walker;
+
+	error = git_revwalk_new(&walker, repo);
+	error = git_revwalk_push_head(walker);
+	git_revwalk_sorting(walker, GIT_SORT_TIME | GIT_SORT_REVERSE);
+
+	git_oid oid;
+	int i = 0;
+	while (!git_revwalk_next(&oid, walker)) {
+		std::cerr << "REV# " << ++i << "\n";
+	}
 }
