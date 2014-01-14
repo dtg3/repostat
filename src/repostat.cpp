bf3c74742d349a0f9f9f90cbd3c06e8aa29ea7f1
Evan Purkhiser <evanpurkhiser@gmail.com>
2014-01-14 03:41:25 -0500

    Add comments and cleanup whitespace

--- src/repostat.cpp
+++ src/repostat.cpp
@@ -9,31 +9,35 @@
 
 #define REPOS_DIR "repos"
 
-
 int main(int argc, char * argv[]) {
 
 	// Look for the repository directory
 	struct stat st;
 	stat(REPOS_DIR, &st);
 
-	// Create directory to store repos
+	// Create directory to store repositories
 	if ( ! S_ISDIR(st.st_mode) && mkdir(REPOS_DIR, 0755) < 0) {
 		perror("Error creating repository directory");
 		return 0;
 	}
 
 	git_repository *repo = NULL;
-	const char *url = "https://github.com/octocat/Spoon-Knife";
+	const char *url  = "https://github.com/octocat/Spoon-Knife";
 	const char *path = "repos/spoon";
 
-	// Clone repo, and open it
+	// Clone repository and open it. `git_clone` _does_ open it for us, but if
+	// it fails (i.e. the repo exists) it wont open it. There's no harm in
+	// re-opening the repo after cloning.
 	git_clone(&repo, url, path, NULL);
 	git_repository_open(&repo, path);
 
 	git_revwalk *walker;
 
+	// Setup revision walker to traverse the git repository directed graph of
+	// commits in topological order. This will ensure we traverse through all
+	// commits including merges
 	git_revwalk_new(&walker, repo); // Returns Error Code
-	git_revwalk_push_head(walker); // Returns Error Code
+	git_revwalk_push_head(walker);  // Returns Error Code
 	git_revwalk_sorting(walker, GIT_SORT_TOPOLOGICAL);
 
 	git_oid oid1;
@@ -45,10 +49,12 @@ int main(int argc, char * argv[]) {
 	git_diff *diff;
 	git_patch *patch;
 
-	// Pop the very first commit
+	// Grab the first object ID
 	git_revwalk_next(&oid1, walker);
 
+	// Iterate over every commit. Currently this will miss the last commit
 	while ( ! git_revwalk_next(&oid2, walker)) {
+
 		// Lookup this commit and the parent
 		git_commit_lookup(&commit1, repo, &oid1);
 		git_commit_lookup(&commit2, repo, &oid2);
@@ -57,16 +63,19 @@ int main(int argc, char * argv[]) {
 		git_commit_tree(&tree1, commit1);
 		git_commit_tree(&tree2, commit2);
 
-		// Do diff between the two trees and create a patch
+		// Do a diff between the two trees and create a patch
 		git_diff_tree_to_tree(&diff, repo, tree1, tree2, NULL);
 		git_patch_from_diff(&patch, diff, 0);
 
 		std::cerr << "COMMIT " << git_oid_allocfmt(&oid1) << '\n';
 
+		// Move to the next object ID
 		oid1 = oid2;
 	}
 	
+	// Clean up memory
 	git_revwalk_free(walker);
 	git_repository_free(repo);
+
 	return 0;
 }
