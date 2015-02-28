6c839a4a6ed53f763ea624a7441a8bd6c01f4bab
Drew Guarnera <dtg3@zips.uakron.edu>
2015-02-27 19:24:10 -0500

    Add argparse to support repository path

--- gitty.py
+++ gitty.py
@@ -1,12 +1,19 @@
 #run me from the git repo root
 
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('repository')
+args = parser.parse_args()
+
 
 graph = open("graph.dot", "wb")
 graph.write('digraph G {\n')
 graph.write('\trankdir="BT"\n')
 
-output = subprocess.check_output(['git', 'rev-list', '--remotes', '--pretty=format:"%h %p"']).splitlines()
+
+output = subprocess.check_output(['git', '--git-dir', args.repository + '/.git', 'log', '--branches', '--pretty=format:"%H %P"']).splitlines()
 for line in output:
 	if not 'commit' in line.strip().strip("\""):
 		SHAS = line.strip().strip("\"").split(' ')
