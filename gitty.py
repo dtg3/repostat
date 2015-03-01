9e7ad0c5a1f4c27243dfe570108a3837d09825f0
Heather Michaud <hmm34@zips.uakron.edu>
2015-03-01 13:49:16 -0500

    Squish right.
    Re-BFS to write graph. Delete and re-add weighted edges when
    encountering linear paths.

--- gitty.py
+++ gitty.py
@@ -39,8 +39,11 @@ def is_init(sha):
 	return sha == "NULL"    # first commit
 
 def is_linear(sha):
-	# todo: this may be wrong
 	return not is_init(sha) and not is_merge(sha) and not is_branch(sha)
+
+# pre-condition: sha is linear
+def is_first_linear(sha):
+	return not is_linear(dc[sha][0])
 
 def is_force_write(sha):
 	return commits[sha]._write
@@ -57,6 +60,12 @@ def is_any_parent_force_write(sha):
 def is_squishable(sha):
 	# todo: implement
 	return False
+
+#def identify(sha):
+#	if is_branch(sha):
+#		commits[sha]._type = 'b'
+#	if is_merge(sha):
+#		commits[sha].
 
 
 def debug_what_am_i(sha):
@@ -75,12 +84,13 @@ def debug_what_am_i(sha):
 class Data(object):
 	def __init__(self):
 		self._write = False
+		self._type = ''
 
 
 # edges used for squashing linear branches
 class Edge(object):
 	_parent = ""
-	_weight = 0
+	_weight = 1
 
 	def __init__(self, p, weight):
 		self._parent = p
@@ -103,6 +113,7 @@ dc = defaultdict(list) # dictionary where keys are child commits
 commits = {}           # dictionary of all commits, whether they should be written
 
 commits["NULL"] = Data()
+commits["NULL"]._write = True
 for line in output:
 	SHAS = line.strip("\"").split(' ')
 
@@ -121,7 +132,7 @@ for line in output:
 cache = dict() # cache of unwritted linear squashes (farthest parent -> original child, weight)
 graph = init_graph() # dot graph
 
-# re-traverse for writing to the file, starting with the first commit using BFS
+# re-traverse for marking what to write to the file, starting with the first commit using BFS
 visited, queue = set(), ["NULL"]
 while queue:
 	node = queue.pop(0)
@@ -130,34 +141,49 @@ while queue:
 
 		debug_what_am_i(node)
 
-		# todo: squish
-		force_write(node)
-
 		#if is_branch(node) or is_merge(node) or is_init(node) or is_orphan(node):
-		#	force_write(node)
+			#force_write(node)
 
-		if is_squishable(node):
+		if is_linear(node):
 			parent = dc[node][0]
-			if parent in cache:
-				temp = cache[parent]
-				del cache[parent]
-				cache[node] = Edge(temp._parent, temp._weight + 1)
-			else:
-				cache[node] = Edge(node, 0)
-			print "squishing " + node + " to " + cache[node]._parent + " with weight " + str(cache[node]._weight)
+			if is_first_linear(node):
+				# create edge from parent ending at current node, weight = 1
+				cache[node] = Edge(parent, 1)
 
+				# remove old edge, put new edge ending at this commit (no change)
+
+			else:
+				# extend parent's squished parent to end at current node, weight + 1
+				temp = cache[parent]
+				cache[node] = Edge(temp._parent, temp._weight + 1)
+				del cache[parent]
+
+				# remove old edge, put new edge ending at this commit
+				dp[temp._parent].remove(parent)
+				dp[temp._parent].append(node)
 
 		# then visit the children of this commit
 		for n in dp[node]:
 			queue.append(n)
 
-# write out only those commits that we marked should be written
-for c in commits:
-	if commits[c]._write:
-		for child in dp[c]:
-			gwrite(graph, child, c, 1)
-	elif c in cache:
-		gwrite(graph, c, cache[c]._parent, cache[c]._weight)
+# re-traverse squished graph to write to file
+visited, queue = set(), ["NULL"]
+while queue:
+	node = queue.pop(0)
+	if node not in visited:
+		visited.add(node)
+
+		# for all children of this commit
+		for child in dp[node]:
+
+			# draw edge in graph
+			weight = 1
+			if child in cache:
+				weight = cache[child]._weight
+			gwrite(graph, child, node, weight)
+
+			# visit
+			queue.append(child)
 
 end_graph(graph)
 
