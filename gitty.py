d0fb9df2f803d3489963b27fc924f5c449be1628
Heather Michaud <hmm34@zips.uakron.edu>
2015-02-28 21:28:08 -0500

    Remove some unused code, add debugging.
    Include graph of current output on test repo, marked with bugs. The full
    un-squished version is in save.pdf. Use shortened hashes for testing.
    
    Remove non-oprhanisim as a rule for is_linear

--- gitty.py
+++ gitty.py
@@ -9,9 +9,9 @@ from collections import defaultdict
 def gwrite(graph, child, parent, weight):
 	if not parent:
 		graph.write('\t"NULL" -> "' + child + '";\n')
-	elif weight <= 1:
+	elif weight == 1:
 		graph.write('\t"' + parent + '" -> "' + child + '";\n')
-	else:
+	elif weight > 1:
 		graph.write('\t"' + parent + '" -> "' + child + '" [label="' + str(weight) + '"];\n')
 
 
@@ -29,7 +29,7 @@ def is_init(sha):
 	return sha == "NULL"
 
 def is_linear(sha):
-	return not is_init(sha) and not is_orphan(sha) and not is_merge(sha) and not is_branch(sha)
+	return not is_init(sha) and not is_merge(sha) and not is_branch(sha)
 
 def is_force_write(sha):
 	return commits[sha]._write
@@ -65,7 +65,7 @@ args = parser.parse_args()
 
 
 # first traversal for mapping parent/child relationship to build up a tree
-output = subprocess.check_output(['git', '--git-dir', args.repository + '/.git', 'log', '--branches', '--pretty=format:"%H %P"']).splitlines()
+output = subprocess.check_output(['git', '--git-dir', args.repository + '/.git', 'log', '--branches', '--pretty=format:"%h %p"']).splitlines()
 dp = defaultdict(list) # dictionary where keys are parent commits
 dc = defaultdict(list) # dictionary where keys are child commits
 commits = {}
@@ -97,6 +97,7 @@ graph.write('\trankdir="BT"\n')
 visited, queue = set(), ["NULL"]
 while queue:
 	node = queue.pop(0)
+	print "CHECKING " + node
 	if node not in visited:
 		visited.add(node)
 
@@ -106,31 +107,42 @@ while queue:
 				print node + " is branch!"
 			elif is_merge(node):
 				print node + " is merge!"
+				for parent in dc[node]:
+					if parent in cache:
+						print "parent is linear!"
+						gwrite(graph, parent, cache[parent]._parent, cache[parent]._weight)
+						del cache[parent]
+					gwrite(graph, node, parent, 1)
 			elif is_init(node):
 				print node + " is init!"
 			elif is_orphan(node):
 				print node + " is orphan!"
 			force_write(node)
+
 		
-		if is_branch(node) or is_linear(node):
+		if not is_init(node):
 			parent = dc[node][0]
-			print node + " is linear!"
-			print "parent: " + parent
-			print "child: " + node
 			if parent in cache:
 				temp = cache[parent]
 				del cache[parent]
 				cache[node] = Edge(temp._parent, temp._weight + 1)
 			else:
-				cache[node] = Edge(node, 1)
+				cache[node] = Edge(node, 0)
 			print "squishing " + node + " to " + cache[node]._parent + " with weight " + str(cache[node]._weight)
+
+			print node + " is linear! parent is: " + cache[node]._parent
 
 			if is_branch(node):
 				gwrite(graph, node, cache[node]._parent, cache[node]._weight)
 				del cache[node]
 
+		# then visit the children of this commit
 		for n in dp[node]:
 			queue.append(n)
+		print "queue is now at: " + str(queue)
+
+	else:
+		print node + " has been checked already"
 
 for c in commits:
 	if commits[c]._write:
@@ -142,75 +154,4 @@ for c in commits:
 
 graph.write('}\n')
 graph.close()
-
-#output = subprocess.check_output(['git', '--git-dir', args.repository + '/.git', 'log', '--branches', '--pretty=format:"%H %P"']).splitlines()
-#for line in output:
-#	SHAS = line.strip("\"").split(' ')
-#
-#	child = SHAS[0]
-#	parents = SHAS[1:]
-#
-#	# TODO: missing something here, or writing something the wrong way.
-#	# the graph is all goofy, and nodes point to nodes they shouldn't.
-#	# investigate.
-#
-#	is_branch = False
-#	for p in parents:
-#		if len(dp[p]) > 1:
-#			is_branch = True
-#
-#	is_init = False
-#	for p in parents:
-#		if not p:
-#			is_init = True
-#
-#	is_linear = False
-#	if len(parents) == 1 and len(dp[parents[0]]) == 1:
-#		is_linear = True
-#
-#	# merge
-#	if len(parents) >= 2:
-#		for p in parents:
-#			gwrite(graph, child, p, 1)
-#			if p in cache:
-#				gwrite(graph, cache[p]._parent, cache[p]._weight)
-#				del cache[p]
-#
-#	# branch
-#	elif is_branch:
-#		for p in parents:
-#			gwrite(graph, child, p, 1)
-#			if p in cache:
-#				gwrite(graph, cache[p]._parent, cache[p]._weight)
-#				del cache[p]
-#
-#	# init
-#	elif is_init:
-#		for p in parents:
-#			gwrite(graph, child, p, 1)
-#			if p in cache:
-#				gwrite(graph, cache[p]._parent, cache[p]._weight)
-#				del cache[p]
-#
-#	# linear
-#	elif is_linear:
-#		p = parents[0]
-#		if child in cache:
-#			cache[p] = Edge(cache[child]._parent, cache[child]._weight + 1)
-#			del cache[child]
-#		else:
-#			cache[p] = Edge(child, 1)
-#
-#	# wtf
-#	else:
-#		print "ABORT! AHH! JUMP SHIP!"
-#
-
-# write anything left remaining in the cache
-#for key in cache:
-#	gwrite(graph, key, cache[key]._parent, cache[key]._weight)
-#
-#graph.write('}\n')
-#graph.close()
-
 
