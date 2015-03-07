cfbd8559ca00ee9b21f1aa5af73e956917192746
Drew Guarnera <dtg3@zips.uakron.edu>
2015-03-06 22:07:17 -0500

    Remove file extension for svg

--- dotter.py
+++ dotter.py
@@ -2,7 +2,7 @@ import subprocess
 
 def draw_graph(inputFile, outputFile):
 
-	dot = subprocess.Popen(["dot", "-v", "-Tsvg", inputFile, "-o", outputFile + ".svg"], 
+	dot = subprocess.Popen(["dot", "-v", "-Tsvg", inputFile, "-o", outputFile], 
 		stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 	while True:
